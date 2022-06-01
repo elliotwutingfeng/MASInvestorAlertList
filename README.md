274eb78a0af878e4842680b069d857e7cebad3c2
Tingfeng <wu.tingfeng@u.nus.edu>
2022-06-01 12:05:03 +0800

    initial commit
    add parliamentary replies
    
    update blocklist
    
    update blocklist
    
    convert hostnames to lowercase
    
    update blocklist
    
    remove blocklist
    
    remove blocklist

---
+++ README.md
@@ -0,0 +1,38 @@
+# MAS Investor Alert List
+
+![Go](https://img.shields.io/badge/Go-00ADD8?style=for-the-badge&logo=go&logoColor=white)
+![GitHub Actions](https://img.shields.io/badge/GitHub_Actions-2088FF?style=for-the-badge&logo=github-actions&logoColor=white)
+
+[![GitHub license](https://img.shields.io/badge/LICENSE-BSD--3--CLAUSE-GREEN?style=for-the-badge)](LICENSE)
+[![Go Report Card](https://goreportcard.com/badge/github.com/elliotwutingfeng/MASInvestorAlertList?style=for-the-badge)](https://goreportcard.com/report/github.com/elliotwutingfeng/MASInvestorAlertList)
+
+Generate a machine-readable `.txt` blocklist of websites affiliated with [unregulated persons](https://www.mas.gov.sg/news/parliamentary-replies/2012/reply-to-parliamentary-question-on-investor-alert-list) who, based on information received by the [Monetary Authority of Singapore (MAS)](https://www.mas.gov.sg/investor-alert-list), may have been wrongly perceived as being licensed or regulated by MAS. This list is not exhaustive and is based on what was known to MAS at the time of publication.
+
+**Disclaimer:** _This project is not sponsored, endorsed, or otherwise affiliated with the Monetary Authority of Singapore._
+
+## Requirements
+
+-   Go >= 1.18
+
+## Setup instructions
+
+`git clone` and `cd` into the project directory, then run the following
+
+```bash
+go mod init dummy && go mod tidy
+```
+
+## Usage
+
+```bash
+go run scraper.go
+```
+
+## Libraries/Frameworks used
+
+-   [fasttld](https://github.com/elliotwutingfeng/go-fasttld)
+-   [xurls](https://github.com/mvdan/xurls)
+
+## Monetary Authority of Singapore Terms of Use
+
+https://www.mas.gov.sg/terms-of-use
---
+++ scraper.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/elliotwutingfeng/go-fasttld"
+	"mvdan.cc/xurls/v2"
+)
+
+func unique(s []string) []string {
+	inResult := make(map[string]bool)
+	var result []string
+	for _, str := range s {
+		if _, ok := inResult[str]; !ok {
+			inResult[str] = true
+			result = append(result, str)
+		}
+	}
+	return result
+}
+
+func main() {
+	urlSchemeRegex := regexp.MustCompile(`^[A-Za-z0-9+-.]+://`)
+	zerowidthRegex := regexp.MustCompile("^[\u200B-\u200D\uFEFF]")
+	whitespaceRegex := regexp.MustCompile("\\s+")
+
+	resp, err := http.Get("https://www.mas.gov.sg/api/v1/ialsearch?json.nl=map&wt=json&rows=10000&q=*:*&sort=date_dt+desc&start=0")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var bodyJSON map[string]interface{}
+	json.Unmarshal([]byte(body), &bodyJSON)
+
+	response, ok := bodyJSON["response"].(map[string]interface{})
+	if !ok {
+		log.Fatalln(`"response" not in bodyJSON`)
+	}
+
+	docs, ok := response["docs"].([]interface{})
+	if !ok {
+		log.Fatalln(`"docs" not in bodyJSON`)
+	}
+
+	links := []string{}
+	rxRelaxed := xurls.Relaxed()
+	for _, doc := range docs {
+		if d, ok := doc.(map[string]interface{}); ok {
+			websiteS, ok := d["website_s"].(string)
+			if !ok {
+				continue
+			}
+			links = append(links, rxRelaxed.FindAllString(websiteS, -1)...)
+		}
+	}
+
+	urls := make(map[string]bool)
+
+	extractor, _ := fasttld.New(fasttld.SuffixListParams{})
+
+	for _, maybeURL := range links {
+		// remove zero-width spaces
+		rawURL := zerowidthRegex.ReplaceAllLiteralString(maybeURL, "")
+		// remove whitespaces
+		rawURL = whitespaceRegex.ReplaceAllLiteralString(rawURL, "")
+		// remove url scheme (e.g. http:// https:// etc.)
+		rawURL = urlSchemeRegex.ReplaceAllString(rawURL, "")
+		// remove whitespace on both ends
+		rawURL = strings.TrimSpace(rawURL)
+		// remove trailing slash
+		rawURL = strings.TrimRight(rawURL, "/")
+
+		// convert hostname to lowercase
+		res := extractor.Extract(fasttld.URLParams{URL: rawURL})
+		subDomain := res.SubDomain
+		registeredDomain := res.RegisteredDomain
+		hostname := registeredDomain
+		if len(subDomain) != 0 {
+			hostname = subDomain + "." + registeredDomain
+		}
+		port := res.Port
+		if len(port) != 0 {
+			port = ":" + port
+		}
+		url := strings.ToLower(hostname) + port + res.Path
+		urls[url] = true
+	}
+
+	// get map keys as slice
+	sortedURLs := make([]string, len(urls))
+	i := 0
+	for k := range urls {
+		sortedURLs[i] = k
+		i++
+	}
+
+	sort.Strings(sortedURLs) // sort alphabetically
+
+	if len(sortedURLs) > 0 {
+		err = os.WriteFile("mas-investor-alert-list.txt", []byte(strings.Join(sortedURLs, "\n")), 0644)
+	} else {
+		log.Fatal("No URLs found")
+	}
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
